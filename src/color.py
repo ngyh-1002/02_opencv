c0facaff00945f12e2f0341b7781f55393c48d3b
ngyh-1002 <01049200297aa@gmail.com>
2025-07-29 10:17:32 +0900

    opencv_bgr 기본코드
    opencv_bgr 기본코드

---
+++ src/color.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+# 기본값
+img = cv2.imread('../img/like_lenna.png')
+# bgr
+bgr = cv2.imread('../img/like_lenna.png', cv2.IMREAD_COLOR)
+# a
+bgra = cv2.imread('../img/like_lenna.png', cv2.IMREAD_UNCHANGED)
+
+# shape값 받기
+print("default", img.shape, "color", bgr.shape, "unchanged", bgra.shape)
+
+cv2.imshow('img', img)
+cv2.imshow('bgr', bgr)
+cv2.imshow('bgra', bgra[:,:,3])
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
